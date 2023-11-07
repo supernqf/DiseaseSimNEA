605bed8b4d8d1c409f4d8dff3d8184531e1e285c
supernqf <117743611+supernqf@users.noreply.github.com>
2023-11-07 12:32:20 +0000

    Updated classes.py
    same changes made as the previous update to the main.py file.

--- classes.py
+++ classes.py
@@ -9,20 +9,32 @@ class Particle:
         self.color = color
         self.vx = random.uniform(-2, 2)
         self.vy = random.uniform(-2, 2)
-
     def infect(self):
         self.state = "infected"
-
-    def move(self, sim_speed):
-        dt = pygame.time.Clock().tick(60) / 1000  
-        self.x += self.vx * sim_speed * dt
-        self.y += self.vy * sim_speed * dt
-
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
     def draw(self, screen):
         if self.state == "infected":
-            color = self.color
+            color = (0, 255, 0)  # Set infected color to green (0, 255, 0)
         elif self.state == "dead":
-            color = (0, 0, 0)
+            color = (0, 0, 0)  # Set dead color to black (0, 0, 0)
         else:
-            color = (128, 128, 128)
+            color = self.color
         pygame.draw.circle(screen, color, (int(self.x), int(self.y)), 5)
+    def check_collision(self, particles, chance_of_infection):
+        if self.state == "unaffected":
+            for particle in particles:
+                if (
+                    self is not particle
+                    and particle.state == "infected"
+                    and self.collides(particle)
+                    and random.random() < chance_of_infection
+                ):
+                    self.infect()
+                    break  # Break the loop once infected by one infected particle
+    def collides(self, particle):
+        dx = particle.x - self.x
+        dy = particle.y - self.y
+        distance = (dx ** 2 + dy ** 2) ** 0.5
+        return distance < 20
