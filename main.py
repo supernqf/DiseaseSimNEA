4196767d69d9c82ae4e41cab3c602e761b1fcc44
supernqf <117743611+supernqf@users.noreply.github.com>
2023-11-07 12:31:38 +0000

    Updated main.py
    I have fixed the collision feature so now if an infected particle gets within 20 pixels of a null particle, it has an inputted chance (chance_of_infection) to change the null particle into an infected particle.

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ if __name__ == "__main__":
     num_particles = 1000
     num_infected_particles = int(input("Enter the number of infected particles: "))
     num_null_particles = num_particles - num_infected_particles
+    chance_of_infection = float(input("Enter the chance of infection (0-1): "))
     for _ in range(num_infected_particles):
         x = random.randint(0, screen_width)
         y = random.randint(0, screen_height)
         overlaps = False
         for particle in particles:
-            if abs(particle.x - x) < 10 and abs(particle.y - y) < 10:
+            if abs(particle.x - x) < 20 and abs(particle.y - y) < 20:
                 overlaps = True
                 break
         if not overlaps:
@@ -30,12 +31,12 @@ if __name__ == "__main__":
         y = random.randint(0, screen_height)
         overlaps = False
         for particle in particles:
-            if abs(particle.x - x) < 10 and abs(particle.y - y) < 10:
+            if abs(particle.x - x) < 20 and abs(particle.y - y) < 20:
                 overlaps = True
                 break
         if not overlaps:
             particles.append(Particle(x, y, NULL_COLOR))
-    sim_speed = float(input("Enter the simulation speed: "))
+
     for i in range(10):
         particles[i].infect()
     running = True
@@ -44,24 +45,12 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 running = False
         screen.fill(WHITE)
+
         for particle in particles:
-            particle.move(sim_speed)
+            particle.move()
+            particle.check_collision(particles, chance_of_infection)  # Pass the chance_of_infection parameter
             particle.draw(screen)
-            for other_particle in particles:
-                if particle is not other_particle:
-                    dx = other_particle.x - particle.x
-                    dy = other_particle.y - particle.y
-                    distance = (dx ** 2 + dy ** 2) ** 0.5
-                    if distance < 10:
-                        particle.vx = -particle.vx
-                        particle.vy = -particle.vy
-                        other_particle.vx = -other_particle.vx
-                        other_particle.vy = -other_particle.vy
-                        if particle.state == "infected" and other_particle.state != "infected":
-                            other_particle.infect()
-                        elif other_particle.state == "infected" and particle.state != "infected":
-                            particle.infect()
         pygame.display.flip()
-        pygame.time.wait(int(100 * sim_speed))
+        pygame.time.wait(100)
     pygame.quit()
 
