0e905940574287159944d0edafbbffe3fbcdadaf
supernqf <117743611+supernqf@users.noreply.github.com>
2023-11-30 14:41:44 +0000

    Update classes.py
    now particles can die

--- classes.py
+++ classes.py
@@ -11,20 +11,27 @@ class Particle:
         self.vy = random.uniform(-2, 2)
         self.screen_width = screen_width
         self.screen_height = screen_height
+        self.time_since_infection = 0  # Track time since infection
     def infect(self):
         self.state = "infected"
+        self.time_since_infection = pygame.time.get_ticks()  # Record the time of infection
+    def die(self):
+        self.state = "dead"
+        self.vx = 0
+        self.vy = 0
     def move(self):
-        self.x += self.vx
-        self.y += self.vy
-        if self.x < 0 or self.x > self.screen_width:
-            self.vx *= -1
-        if self.y < 0 or self.y > self.screen_height:
-            self.vy *= -1
+        if self.state != "dead":  # Dead particles don't move
+            self.x += self.vx
+            self.y += self.vy
+            if self.x < 0 or self.x > self.screen_width:
+                self.vx *= -1
+            if self.y < 0 or self.y > self.screen_height:
+                self.vy *= -1
     def draw(self, screen):
         if self.state == "infected":
-            color = (0, 255, 0)  # Set infected color to green (0, 255, 0)
+            color = (0, 255, 0)  # Set infected color to green
         elif self.state == "dead":
-            color = (0, 0, 0)  # Set dead color to black (0, 0, 0)
+            color = (0, 0, 0)  # Set dead color to black
         else:
             color = self.color
         pygame.draw.circle(screen, color, (int(self.x), int(self.y)), 5)
@@ -37,6 +44,12 @@ class Particle:
                         and random.random() < chance_of_infection):
                     self.infect()
                     break  # Break the loop once infected by one infected particle
+    def check_death(self, death_rate):
+        # Check if enough time has passed (1 second) to consider changing the state to dead
+        if self.state == "infected" and (pygame.time.get_ticks() - self.time_since_infection) >= 1000:
+            if random.random() < death_rate:
+                self.die()
+            self.time_since_infection += 1000  # Reset the time since the last death check
     def collides(self, particle):
         dx = particle.x - self.x
         dy = particle.y - self.y
