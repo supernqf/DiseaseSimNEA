d994b1ccf6ef24fe874235ef786e82783be767d3
supernqf <117743611+supernqf@users.noreply.github.com>
2023-11-30 14:23:01 +0000

    Update main.py
    small changes have been made to adapt to the new display inputs

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-import pygame 
-import random
-from classes import Particle
-from validation import get_valid_integer
-from simulation import start_disease_simulation
-# Initialize pygame
-pygame.init()
-# Set up the display
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Game Menu")
+pygame.display.set_caption("Nath's Disease Simulator")
 # Define colors
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
+input_box_rect = pygame.Rect(50, 100, 140, 32)
+input_box_rect_integer = pygame.Rect(50, 100, 140, 32)  
+input_box_rect_infection = pygame.Rect(50, 150, 140, 32)
+input_box_rect_death_rate = pygame.Rect(50, 200, 140, 32)
+
 # Define button dimensions and positions
 button_width = 200
 button_height = 50
 play_button_pos = (screen_width // 2 - button_width // 2, 200)
 settings_button_pos = (screen_width // 2 - button_width // 2, 300)
 # Set up the font
-font = pygame.font.Font(None, 36)
+font = pygame.font.Font(None, 26)
 # Game Loop
 running = True
+num_infected_particles = 0
+chance_of_infection = 0.0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -32,7 +31,18 @@ while running:
             # Check if Play button is clicked
             if (play_button_pos[0] <= mouse_x <= play_button_pos[0] + button_width and
                     play_button_pos[1] <= mouse_y <= play_button_pos[1] + button_height):
-                start_disease_simulation(screen_width, screen_height, screen)  
+              num_infected_particles, chance_of_infection, death_rate = get_valid_inputs(
+                  screen,
+                  "Enter number of infected particles:",
+                  "Enter chance of infection (0-1):",
+                  "Enter death rate (0-1):",
+                  font,
+                  GREEN,
+                  input_box_rect_integer,
+                  input_box_rect_infection,
+                  input_box_rect_death_rate
+              )
+              start_disease_simulation(screen_width, screen_height, screen, num_infected_particles, chance_of_infection)  
             # Check if Settings button is clicked
             elif (settings_button_pos[0] <= mouse_x <= settings_button_pos[0] + button_width and
                   settings_button_pos[1] <= mouse_y <= settings_button_pos[1] + button_height):
