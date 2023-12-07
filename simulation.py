95fc566120efc22c25ce492729cdd6a0773fa779
supernqf <117743611+supernqf@users.noreply.github.com>
2023-12-07 14:49:49 +0000

    Update simulation.py
    Now a live counter displays at the top left showing the currently infected particles and the currently dead particles

--- simulation.py
+++ simulation.py
@@ -10,9 +10,11 @@ def start_disease_simulation(screen_width, screen_height, screen,
   NULL_COLOR = (128, 128, 128)
   INFECTED_COLOR = (0, 255, 0)
   WHITE = (255, 255, 255)
+  BLACK = (0, 0, 0)
   particles = []
   num_particles = 1000
   num_null_particles = num_particles - num_infected_particles
+  font = pygame.font.Font(None, 24)
   for _ in range(num_infected_particles):
     x = random.randint(0, screen_width)
     y = random.randint(0, screen_height)
@@ -51,26 +53,49 @@ def start_disease_simulation(screen_width, screen_height, screen,
   play_img = pygame.transform.scale(play_img, (50, 50))
   # Main simulation loop
   while sim_running:
-      for event in pygame.event.get():
-          if event.type == pygame.MOUSEBUTTONDOWN:
-              if event.button == 1:
-                  mouse_pos = pygame.mouse.get_pos()
-                  pause_button_rect = pause_img.get_rect(topleft=(screen_width/2 - 25, 0))
-                  if pause_button_rect.collidepoint(mouse_pos):
-                      paused = not paused
-                      if paused:
-                          screen.blit(play_img, (screen_width/2 - 25, 0))  # Change the pause image to play image while paused
-                      else:
-                          screen.blit(pause_img, (screen_width/2 - 25, 0))  # Change the play image back to pause image while unpaused
-      screen.fill(WHITE)
-      for particle in particles:
-          if not paused:  # Only update and draw particles if not paused
-              particle.move()
-              particle.check_collision(particles, chance_of_infection)
-              particle.check_death(death_rate)
-          particle.draw(screen)
-          last_positions.append((particle.x, particle.y))  # Store the last positions of particles
-      # Display the pause button at the top middle of the screen
-      screen.blit(pause_img, (screen_width/2 - 25, 0))
-      pygame.display.flip()
-      pygame.time.wait(100)
+    for event in pygame.event.get():
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1:
+          mouse_pos = pygame.mouse.get_pos()
+          pause_button_rect = pause_img.get_rect(topleft=(screen_width / 2 -
+                                                          25, 0))
+          if pause_button_rect.collidepoint(mouse_pos):
+            paused = not paused
+            if paused:
+              screen.blit(
+                play_img,
+                (screen_width / 2 - 25,
+                 0))  # Change the pause image to play image while paused
+            else:
+              screen.blit(pause_img, (
+                screen_width / 2 - 25,
+                0))  # Change the play image back to pause image while unpaused
+    # drawing the number of infected and dead particles in the top left
+
+    screen.fill(WHITE)
+    infected_text = font.render("Infected: " + str(Particle.infection_count),
+                                True, BLACK)
+    screen.blit(infected_text, (10, 10))
+    dead_text = font.render("Dead: " + str(Particle.death_count), True, BLACK)
+    screen.blit(dead_text, (10, 30))
+    for particle in particles:
+      if not paused:  # Only update and draw particles if not paused
+        particle.move()
+        particle.check_collision(particles, chance_of_infection)
+        particle.check_death(death_rate)
+      particle.draw(screen)
+      last_positions.append(
+        (particle.x, particle.y))  # Store the last positions of particles
+    # Display the pause button at the top middle of the screen
+
+    infected_text = font.render("Infected: " + str(Particle.infection_count),
+                                True, BLACK)
+    screen.blit(infected_text, (10, 10))
+    dead_text = font.render("Dead: " + str(Particle.death_count), True, BLACK)
+    screen.blit(dead_text, (10, 30))
+
+    screen.blit(pause_img, (screen_width / 2 - 25, 0))
+
+    pygame.display.flip()
+    pygame.time.wait(100)
+    
