8142a3fae3924d675b9d8790a174774c758cbab1
supernqf <117743611+supernqf@users.noreply.github.com>
2023-12-19 12:42:58 +0000

    Update validation.py
    added functional presets

--- validation.py
+++ validation.py
@@ -1,9 +1,35 @@
 import pygame
+def displayboxes(screen, font, color):
+  label_font = pygame.font.SysFont(None, 24)  # Font for the labels
 
+  preset1 = pygame.Rect(600, 100, 150, 50)
+  text1 = label_font.render("Ebola", True, color)
 
+  preset2 = pygame.Rect(600, 250, 150, 50)
+  text2 = label_font.render("Measles", True, color)
+
+  preset3 = pygame.Rect(600, 400, 150, 50)
+  text3 = label_font.render("Corona Virus", True, color)
+
+  presetboxes = [(preset1, text1),
+                (preset2, text2),
+                (preset3, text3)]
+
+  for box, text in presetboxes:
+      pygame.draw.rect(screen, color, box, 2)
+      screen.blit(text, (box.x + 10, box.y + 10))
+
+  return presetboxes
+def switch_active_box(active, input_boxes, mouse_pos):
+  for index, box in enumerate(input_boxes):
+    if box.collidepoint(mouse_pos):
+        return index
+  return active
+
+  # Updated main function incorporating labeled box functionality
 def get_valid_inputs(screen, prompt_integer, prompt_infection,
-                     prompt_death_rate, font, color, input_box_rect_integer,
-                     input_box_rect_infection, input_box_rect_death_rate):
+                    prompt_death_rate, font, color, input_box_rect_integer,
+                    input_box_rect_infection, input_box_rect_death_rate):
   input_screen = pygame.display.set_mode((800, 600))
   input_str_integer = ''
   input_str_infection = ''
@@ -20,8 +46,61 @@ def get_valid_inputs(screen, prompt_integer, prompt_infection,
   pygame.draw.rect(input_screen, color, input_box_rect_infection, 2)
   input_screen.blit(base_surface_death_rate, (50, 550))
   pygame.draw.rect(input_screen, color, input_box_rect_death_rate, 2)
+
+    # Display labeled boxes
+  presetboxes = displayboxes(input_screen, font, color)
   pygame.display.flip()
+  
   while True:
+    mouse_pos = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    for box, _ in presetboxes:
+      if box.collidepoint(mouse_pos):
+          if click[0] == 1:  # Left mouse button clicked
+              if box == presetboxes[0][0]:  # Placeholder values for Default 1
+                  input_str_integer = '50'
+                  input_str_infection = '0.5'
+                  input_str_death_rate = '0.1'
+              elif box == presetboxes[1][0]:  # Placeholder values for Default 2
+                  input_str_integer = '100'
+                  input_str_infection = '0.3'
+                  input_str_death_rate = '0.05'
+              elif box == presetboxes[2][0]:  # Placeholder values for Default 3
+                  input_str_integer = '25'
+                  input_str_infection = '0.7'
+                  input_str_death_rate = '0.2'
+
+    # Redraw input boxes with updated placeholder values
+    input_screen.fill((0, 0, 0))
+    input_screen.blit(base_surface_integer, (50, 150))
+    text_surface_integer = font.render(input_str_integer, True, color)
+    input_screen.blit(text_surface_integer, (input_box_rect_integer.x + 5, input_box_rect_integer.y + 5))
+    pygame.draw.rect(input_screen, color, input_box_rect_integer, 2)
+
+    input_screen.blit(base_surface_infection, (50, 350))
+    text_surface_infection = font.render(input_str_infection, True, color)
+    input_screen.blit(text_surface_infection, (input_box_rect_infection.x + 5, input_box_rect_infection.y + 5))
+    pygame.draw.rect(input_screen, color, input_box_rect_infection, 2)
+
+    input_screen.blit(base_surface_death_rate, (50, 550))
+    text_surface_death_rate = font.render(input_str_death_rate, True, color)
+    input_screen.blit(text_surface_death_rate, (input_box_rect_death_rate.x + 5, input_box_rect_death_rate.y + 5))
+    pygame.draw.rect(input_screen, color, input_box_rect_death_rate, 2)
+
+    # Redraw labeled boxes
+    presetboxes = displayboxes(input_screen, font, color)
+
+    pygame.display.flip()
+    pygame.time.wait(100)
+    if click[0] == 1:
+      if input_box_rect_integer.collidepoint(mouse_pos):
+          active = "integer"
+      elif input_box_rect_infection.collidepoint(mouse_pos):
+          active = "infection"
+      elif input_box_rect_death_rate.collidepoint(mouse_pos):
+          active = "death_rate"
+
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
@@ -86,6 +165,18 @@ def get_valid_inputs(screen, prompt_integer, prompt_infection,
             input_str_infection += event.unicode
           elif active == "death_rate":
             input_str_death_rate += event.unicode
+          elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+          # Handle switching between input boxes by mouse click
+            active = switch_active_box(active, [input_box_rect_integer,       input_box_rect_infection, input_box_rect_death_rate], mouse_pos)
+
+          # Handle input for each input box
+            if input_box_rect_integer.collidepoint(mouse_pos):
+              active = "integer"
+            elif input_box_rect_infection.collidepoint(mouse_pos):
+              active = "infection"
+            elif input_box_rect_death_rate.collidepoint(mouse_pos):
+                active = "death_rate"
         input_screen.fill((0, 0, 0))
         input_screen.blit(base_surface_integer, (50, 150))
         text_surface_integer = font.render(input_str_integer, True, color)
