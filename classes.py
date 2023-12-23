696db50bd8c74cf297c84e45ed7ad66229ed453b
supernqf <117743611+supernqf@users.noreply.github.com>
2023-12-23 14:01:42 +0000

    Update classes.py

--- classes.py
+++ classes.py
@@ -7,29 +7,32 @@ import validation
 class Particle:
 
   def __init__(self, x, y, color, screen_width, screen_height):
+    # Initialize Particle attributes
     self.x = x
     self.y = y
     self.state = "unaffected"
     self.color = color
-    self.vx = random.uniform(-2, 2)
-    self.vy = random.uniform(-2, 2)
+    self.vx = random.uniform(-2, 2)  # Velocity in x-direction
+    self.vy = random.uniform(-2, 2)  # Velocity in y-direction
     self.screen_width = screen_width
     self.screen_height = screen_height
     self.time_since_infection = 0
+    self.infection_duration = 6000  # Time in milliseconds for infection to progress
 
   death_count = 0
   infection_count = 0
-
-  survival_count = 0  # Class variable to count the number of infections
+  survival_count = 0
 
   def infect(self):
-    if self.state == "unaffected":  # Only increment infection count if particle is not already infected
+    # Infect the particle if it's currently unaffected
+    if self.state == "unaffected":
       Particle.infection_count += 1
     self.state = "infected"
     self.time_since_infection = pygame.time.get_ticks()
 
   def die(self):
-    if self.state != "dead" and self.state != "survived":  # Only increment death count if particle is not already dead or survived
+    # Set particle state to 'dead'
+    if self.state != "dead" and self.state != "survived":
       Particle.death_count += 1
       Particle.infection_count -= 1
     self.state = "dead"
@@ -37,7 +40,8 @@ class Particle:
     self.vy = 0
 
   def survive(self):
-    if self.state != "survived" and self.state != "dead":  # Only increment survived count if particle is not already dead or survived
+    # Set particle state to 'survived'
+    if self.state != "survived" and self.state != "dead":
       Particle.survival_count += 1
       Particle.infection_count -= 1
     self.state = "survived"
@@ -45,7 +49,8 @@ class Particle:
     self.vy = 0
 
   def move(self):
-    if self.state != "dead":  # Dead particles don't move
+    # Move the particle within screen boundaries
+    if self.state != "dead":
       self.x += self.vx
       self.y += self.vy
       if self.x < 0 or self.x > self.screen_width:
@@ -54,10 +59,11 @@ class Particle:
         self.vy *= -1
 
   def draw(self, screen):
+    # Draw the particle based on its state
     if self.state == "infected":
-      color = (0, 255, 0)  # Set infected color to green
+      color = (0, 255, 0)
     elif self.state == "dead":
-      color = (0, 0, 0)  # Set dead color to black
+      color = (0, 0, 0)
     elif self.state == "survived":
       color = (173, 216, 230)
     else:
@@ -65,33 +71,42 @@ class Particle:
     pygame.draw.circle(screen, color, (int(self.x), int(self.y)), 5)
 
   def check_collision(self, particles, chance_of_infection):
+    # Check collision with other particles for possible infection
     if self.state == "unaffected":
       for particle in particles:
-        if (self is not particle and particle.state == "infected"
-            and self.collides(particle)
-            and random.random() < chance_of_infection):
+        if self is not particle and particle.state == "infected" and self.collides(
+            particle) and random.random() < chance_of_infection:
           self.infect()
-          break  # Break the loop once infected by one infected particle
+          break
 
   def check_status(self, death_rate, survival_rate):
-    generaltickspeed = 30000
-
-    # Check if enough time has passed (1 second) to consider changing the state to dead
-    if self.state == "infected" and (pygame.time.get_ticks() -
-                                     self.time_since_infection) >= 1000:
+    # Check the status of an infected particle over time
+    if self.state == "infected" and (
+        pygame.time.get_ticks() -
+        self.time_since_infection) >= self.infection_duration:
       if random.random() < death_rate:
         self.die()
       self.time_since_infection += 1000
       if death_rate != 1:
-        if self.time_since_infection >= generaltickspeed * (
+        if self.time_since_infection >= self.infection_duration * (
             1 / survival_rate) and death_rate != 0 and death_rate != 1:
           self.survive()
-        if death_rate == 0 and self.time_since_infection >= generaltickspeed:
+        if death_rate == 0 and self.time_since_infection >= self.infection_duration:
           self.survive()
 
   def collides(self, particle):
+    # Check if the particle collides with another particle
     dx = particle.x - self.x
     dy = particle.y - self.y
     distance = (dx**2 + dy**2)**0.5
     return distance < 20
+
+  def check_interaction(self, other_particles, chance_of_infection):
+    # Check interaction with other particles for possible infection
+    if self.state == "unaffected":
+      for particle in other_particles:
+        if self is not particle and particle.state == "infected" and self.collides(
+            particle) and random.random() < chance_of_infection:
+          self.infect()
+          break
 
